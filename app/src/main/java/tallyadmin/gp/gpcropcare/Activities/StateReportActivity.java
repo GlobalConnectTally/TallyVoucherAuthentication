04fd513b9bef9ee899bc484a66543147e993006f
MGWMAN006 <maneno.mgwami@gmail.com>
2022-11-15 11:10:11 +0300

    started to design screen for stock report

---
+++ app/src/main/java/tallyadmin/gp/gpcropcare/Activities/StateReportActivity.java
@@ -0,0 +1,14 @@
+package tallyadmin.gp.gpcropcare.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StateReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_state_report);
+    }
+}
---
+++ app/src/main/java/tallyadmin/gp/gpcropcare/Activities/StockReportActivity.java
@@ -0,0 +1,14 @@
+package tallyadmin.gp.gpcropcare.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StockReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_report);
+    }
+}
